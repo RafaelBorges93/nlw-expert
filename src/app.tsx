62ff0c3ab502fdf0c251ae0f5bae67afe0d937cd
RaafelBorges93 <r.borges_19@hotmail.com>
2024-02-06 11:00:39 -0300

    ✨ feat: completed class 2

--- src/app.tsx
+++ src/app.tsx
@@ -18,10 +18,20 @@ export function App() {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard
+          note={{
+            date: new Date(),
+            content:
+              "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem iste, recusandae at voluptas ipsa, eligendi vitae rerum molestias animi doloribus harum dolores sequi quisquam eaque, magni deleniti nisi cumque fugiat!",
+          }}
+        />
+        <NoteCard
+          note={{
+            date: new Date(),
+            content:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, expedita maxime libero alias dolor iure doloremque incidunt commodi optio omnis beatae dolore? Consequatur atque suscipit harum distinctio odio dolor debitis.",
+          }}
+        />
       </div>
     </div>
   );
